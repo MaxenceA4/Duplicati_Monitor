027eb200c0b09452575b813d9dd6320446056ecb
sacifym <76045198+sacifym@users.noreply.github.com>
2023-08-17 22:00:31 +0200

    Made the code clear I translated all my comments and made code clearer (got rid of a lot of echo)

--- index.php
+++ index.php
@@ -11,48 +11,41 @@
 <?php
 global $connection;
 include 'LoginConnection/connection.php';
-// Retrieve all the data from the "example" table
 $query = "SELECT * FROM backup_reports";
 $result = mysqli_query($connection, $query);
 
-
 if (mysqli_num_rows($result) > 0) {
-    echo '<table>';
-    echo '<thead><tr></th><th>Date</th><th>Opération</th><th>Résultat</th>><th>Name Of Computer</th></tr></thead>';
-    echo '<tbody>';
-
+    $tableRows = '';
     $complete = 0;
-
     $date = date('Y-m-d H:i:s');
     $date24h = date('Y-m-d H:i:s', strtotime('-24 hours'));
     $date7j = date('Y-m-d H:i:s', strtotime('-7 days'));
 
-
     while ($row = mysqli_fetch_assoc($result)) {
         $complete++;
-        // si la date rowdate est comprise entre ajd et il y a 24h, on affiche le rapport en vert
+        $class = 'red';
         if ($row['date'] > $date24h && $row['date'] < $date) {
-            echo '<tr class="green">';
+            $class = 'green';
         } else if ($row['date'] > $date7j && $row['date'] < $date24h) {
-            echo '<tr class="orange">';
-        } else {
-            echo '<tr class="red">';
+            $class = 'orange';
         }
-
-        echo '<td>' . $row['date'] . '</td>';
-        echo '<td>' . $row['operation'] . '</td>';
-        echo '<td>' . $row['result'] . '</td>';
-        echo '<td>' . $row['nameOfComputer'] . '</td>';
-        echo '</tr>';
+        $tableRows .= "<tr class=\"$class\"><td>{$row['date']}</td><td>{$row['operation']}</td><td>{$row['result']}</td><td>{$row['nameOfComputer']}</td></tr>";
     }
-    echo '</tbody>';
-    echo '</table>';
-    echo '<p>' . $complete . ' rapport(s) de sauvegarde trouvé(s).</p>';
+
+    $html = "<table>
+            <thead><tr><th>Date</th><th>Opération</th><th>Résultat</th><th>Name Of Computer</th></tr></thead>
+            <tbody>{$tableRows}</tbody>
+            </table>
+            <p>{$complete} backup reports found.</p>";
+
+    echo $html;
+
     $everyone = [];
     $sauve24 = [];
     $sauve7 = [];
     $fautif24 = [];
     $fautif7 = [];
+
     $query = "SELECT * FROM backup_reports";
     $result = mysqli_query($connection, $query);
     while ($row = mysqli_fetch_assoc($result)) {
@@ -60,7 +53,8 @@ if (mysqli_num_rows($result) > 0) {
     }
 
 
-    // Nombre de rapports de sauvegarde trouvés dans les dernières 24 heures
+
+    // Number of backup reports found in the last 24 hours
     $query = "SELECT * FROM backup_reports WHERE date > DATE_SUB(NOW(), INTERVAL 1 DAY)"; //
     $result24h = mysqli_query($connection, $query);
     while ($row = mysqli_fetch_assoc($result24h)) {
@@ -69,8 +63,7 @@ if (mysqli_num_rows($result) > 0) {
     $sauve24 = array_unique($sauve24);
     $fautif24 = array_diff($everyone, $sauve24);
 
-
-    // Nombre de rapports de sauvegarde trouvés dans les dernières 7 jours
+    // Number of backup reports found in the last 7 days
     $query = "SELECT * FROM backup_reports WHERE date > DATE_SUB(NOW(), INTERVAL 7 DAY)";
     $result7j = mysqli_query($connection, $query);
     while ($row = mysqli_fetch_assoc($result7j)) {
@@ -80,13 +73,12 @@ if (mysqli_num_rows($result) > 0) {
     $fautif7 = array_diff($everyone, $sauve7);
 
 
-    // si on veut changer l'intervalle de temps, il faut changer le chiffre 1 dans la ligne ci-dessus
-    // 1 = 24 heures, 7 = 7 jours, 30 = 30 jours, etc.
-    // exemple : $query = "SELECT * FROM backup_reports WHERE date > DATE_SUB(NOW(), INTERVAL 30 DAY)";
+    // If we want to change the interval, we can change the number in the query
+    // 1 = 24 hours, 7 = 7 days, 30 = 30 days, etc.
+    // example : $query = "SELECT * FROM backup_reports WHERE date > DATE_SUB(NOW(), INTERVAL 30 DAY)";
 
-    echo '<p>' . mysqli_num_rows($result24h) . ' rapport(s) de sauvegarde trouvé(s) dans les dernières 24 heures.</p>';
-    // Nombre de rapports de sauvegarde trouvés dans les dernières 7 jours
-    echo '<p>' . mysqli_num_rows($result7j) . ' rapport(s) de sauvegarde trouvé(s) dans les derniers 7 jours.</p>';
+    echo '<p>' . mysqli_num_rows($result24h) . ' number of backups found in the last 24 hours.</p>';
+    echo '<p>' . mysqli_num_rows($result7j) . ' number of backups found in the last 7 days.</p>';
 
     $percentage24h = round(mysqli_num_rows($result24h) / $complete * 100, 2);
     $percentage7j = round(mysqli_num_rows($result7j) / $complete * 100, 2);
@@ -96,13 +88,13 @@ if (mysqli_num_rows($result) > 0) {
     echo '<dialog id="d">
             <form method="dialog">';
     if (is_array($fautif24) || is_object($fautif24))
-        echo '<p>Les ordinateurs suivants n\'ont pas envoyé de rapport de sauvegarde dans les dernières 24 heures :</p>
+        echo '<p>Those computer did not made backups in the last 24 hours:</p>
 <ul>';
     foreach ($fautif24 as $value)
         echo '<li>' . $value . '</li>';
     echo '</ul>';
     if (is_array($fautif7) || is_object($fautif7))
-        echo '<p>Les ordinateurs suivants n\'ont pas envoyé de rapport de sauvegarde dans les dernières 7 jours :</p>
+        echo '<p>Those computer did not made backups in the last 7 days:</p>
 <ul>';
     foreach ($fautif7 as $value)
         echo '<li>' . $value . '</li>';
@@ -112,7 +104,7 @@ if (mysqli_num_rows($result) > 0) {
     echo '<button class="bouton"> Close </button>
             </form>
         </dialog>';
-    echo '<button onclick="d.showModal()" class="bouton"> Montrer les fautifs</button>';
+    echo '<button onclick="d.showModal()" class="bouton"> Show Details</button>';
     // Nombre de rapports dans les dernières 7 jours sur nombre total de rapports
 
     echo '<div class="big-container">';
@@ -121,18 +113,19 @@ if (mysqli_num_rows($result) > 0) {
     echo '<span class="progress-value">' . $percentage24h . '%</span>';
     echo '</div>';
 
-    echo '<span class ="text">Sauvegarde dans les dernières 24 heures.</span>';
+    echo '<span class ="text">Saves in the last 24 hours.</span>';
 
     echo '</div>';
     echo '</div>';
 
 
 } else {
-    echo 'Aucun rapport de sauvegarde trouvé.';
+    echo 'No backup reports found.';
 }
 
 mysqli_close($connection);
 ?>
+
 
 <script>
     var percentage24 = <?php echo $percentage24h; ?>;
